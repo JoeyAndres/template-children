74986c91075dbaf8ef031380e81e19d0340a59db
Joey Arnold <jandres@ualberta.ca>
2016-01-18 01:32:05 -0700

    changed back mutationobserver to all:true to capture all events

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "jandres:template-children",
   summary: "Meteor package that adds the ability to keep track of underlying templates, including ordering.",
-  version: "0.0.2",
+  version: "0.0.3",
   git: "https://github.com/JoeyAndres/template-children.git"
 });
 
